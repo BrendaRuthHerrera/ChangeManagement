0c1cea3ce54a0508476b4a8b24a29c7dedf1c35d
Brenda <brendaruthherrera@gmail.com>
2024-07-22 17:35:41 -0300

    fix styles footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,19 @@
 import '../styles/Footer.css'
 export default function Footer() {
-    return(
-        
-    <div className="Footer">
-        <div className='contentfooter'>
+    return (
+        <div className="Footer">
+            <div className='footer-content'>
+                <div id='but' className='col'>
+                    <h3>Soporte Técnico</h3>
+                </div>
+                <div className='col'>
+                    &copy; 2024 Cirion Technologies. Todos los derechos reservados.
+                </div>
+                <div id='but' className='col'>
+                    <h3>Sugeréncias</h3>
+                </div>
+
+            </div>
         </div>
-                
-     </div>
     )
 }
